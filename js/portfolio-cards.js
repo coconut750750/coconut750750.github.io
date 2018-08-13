a9dd4e1f294095434be3d8f46dacf29a0dbb9a75
Brandon Wang <sourbrandon@gmail.com>
2018-08-13 15:11:31 -0500

    added more project descriptions

--- js/portfolio-cards.js
+++ js/portfolio-cards.js
@@ -4,12 +4,16 @@ jQuery(document).ready(function($) {
     });
 
     function adjustCardWidth() {
-        if ($(window).width() <= 900) {
-            $(".horizontal").css("display", "none");
-            $(".vertical").css("display", "flex");
+        if ($(window).width() < 768) {
+            $(".horizonal").removeClass("horizontal flex-md-row").addClass("vertical");
+            $(".horizontal_img").css("display", "none");
+            $(".vertical_img").css("display", "block");
+            $(".card-body").addClass("col");
         } else {
-            $(".horizontal").css("display", "flex");
-            $(".vertical").css("display", "none");
+            $(".vertical").addClass("horizontal flex-md-row").removeClass("vertical");
+            $(".vertical_img").css("display", "none");
+            $(".horizontal_img").css("display", "block");
+            $(".card-body").removeClass("col");
         }
     }
     adjustCardWidth();
